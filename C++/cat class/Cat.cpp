6a9be83c7b85a87709567ac5101e351c222e0fab
henrical <bloodshotdawn55@gmail.com>
2014-09-28 19:04:26 -0300

    fixed stringstream bug (std::stringstreamgit status)

--- C++/cat class/Cat.cpp
+++ C++/cat class/Cat.cpp
@@ -3,19 +3,19 @@
 #include <sstream>
 
 //============CLASS METHODS================
-Cat::Cat(string name, int age, float weight)
+Cat::Cat(std::string name, int age, float weight)
 {
 	_name = name;
 	_age = age;
 	_weight = weight;
 }
 
-void Cat::setName(string name)
+void Cat::setName(std::string name)
 {
 	_name = name;
 }
 
-string Cat::getName()
+std::string Cat::getName()
 {
 	return _name;
 }
@@ -30,27 +30,21 @@ float Cat::getWeight()
 	return _weight;
 }
 
-string Cat::printCat()
+std::string Cat::printCat()
 {
-	string result;
-	stringstream stm;
-	stringstream stm2;
-
-	cout << getName() <<endl;
-
-
-
+	std::string result;
+	std::stringstream stm;
+	
+	stm << getName();
+	stm << ", ";
 	stm << getAge();
-	result = stm.str(); 
-		
-	cout << result <<endl;
+	stm << ", ";
+	stm << getWeight();
+	stm << std::endl;
 
+	result = stm.str();
 
-
-	stm2 << getWeight();
-	result = stm2.str();
-
-	cout << result <<endl;
+	return result;
 }
 
 
@@ -59,8 +53,8 @@ string Cat::printCat()
 int main()
 {
 	Cat c("Fuckerton", 3, (float)13.5);
-	c.printCat();
+	std::cout << c.printCat();
 	
-	cout << "Success!"<<endl;
+	std::cout << "Success!" << std::endl;
 }
 
--- C++/cat class/Cat.h
+++ C++/cat class/Cat.h
@@ -1,24 +1,22 @@
 #ifndef CAT_H
 #define CAT_H
 
-using namespace std;
-
 class Cat
 {	
 	private:
-		string _name;
+		std::string _name;
 		int _age;
 		float _weight;
 	public:
-		Cat(string, int, float);
+		Cat(std::string, int, float);
 
-		void setName(string name);
+		void setName(std::string name);
 
-		string getName();
+		std::string getName();
 		int getAge();
 		float getWeight();
 
-		string printCat();
+		std::string printCat();
 };
 
 #endif //CAT_H
