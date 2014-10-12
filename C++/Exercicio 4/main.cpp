381f8357e95897da513c34f164de55cca9e4b819
henrical <bloodshotdawn55@gmail.com>
2014-10-12 09:05:58 -0300

    minor changes

--- C++/Exercicio 4/main.cpp
+++ C++/Exercicio 4/main.cpp
@@ -9,14 +9,14 @@
 int main()
 {
 	
-	EconomicEngine *eE1 = new EconomicEngine();
-	EconomicEngine eE2();
-	XtremeEngine xE1();
-	XtremeEngine xE2();
+	//EconomicEngine *eE1 = new EconomicEngine();
+	EconomicEngine *eE2();
+	//XtremeEngine *xE1 = new XtremeEngine();
+	//XtremeEngine *xE2 = new XtremeEngine();
 
 
-	EconomicCar eC(*eE1);
-	//MediumCar mC = new MediumCar(eE2);
+	//EconomicCar eC(*eE1);
+	MediumCar mC = new MediumCar(eE2);
 	
 
 }
